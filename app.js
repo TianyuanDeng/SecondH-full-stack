83b31952e252264a7e506e9a67bee0d824cb9033
Tianyuan Deng <td337@mail.missouri.edu>
2019-04-22 19:59:49 -0500

    Fixed DeprecationWarning

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if(isProduction){
   mongoose.connect(process.env.MONGODB_URI);
 } else {
   mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true });
-  mongoose.set('debug', true);
+  mongoose.set('useCreateIndex', true);
 }
 
 require('./models/User');
